e0c15660b8ae19a444b0981343b839c3c2360515
Huihuo Zheng <huihuo.zheng@anl.gov>
2026-01-30 07:12:49 -0600

    added results

--- 04_benchmarks/mpi_pingpong/run_pbs.sh
+++ 04_benchmarks/mpi_pingpong/run_pbs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #PBS -N mpi_pingpong
-#PBS -l select=2:ncpus=1:mpiprocs=1
+#PBS -l select=2
 #PBS -l walltime=00:05:00
 #PBS -j oe
-#PBS -o pingpong_${PBS_JOBID}.out
 #PBS -A datascience
 #PBS -l filesystems=home:eagle
 
@@ -13,7 +12,7 @@
 
 set -euo pipefail
 
-cd "${PBS_O_WORKDIR}"
+cd ${PBS_O_WORKDIR}
 
 # Run (2 ranks on 2 nodes)
-mpiexec -np 2 --ppn 1 ./pingpong --min 1 --max 8M --iters 1000 --warmup 100
+mpiexec -np 2 --ppn 1 ./pingpong --min 1 --max 64M --iters 1000 --warmup 100
