67db7b318a78fc5f140e3083d11f7093bfde41ab
Huihuo Zheng <zhenghh04@gmail.com>
2026-03-16 03:52:14 +0000

    update main.py

--- 07_gpu/00_motivation/main.py
+++ 07_gpu/00_motivation/main.py
@@ -4,8 +4,9 @@ import time
 
 import torch
 
-matrix_size = 2048
-
+matrix_size = 8192
+niter=10
+warmup=2
 
 def benchmark(device_name: str, warmup: int, niter: int) -> float:
     device = torch.device(device_name)
@@ -35,16 +36,16 @@ def benchmark(device_name: str, warmup: int, niter: int) -> float:
     end = time.perf_counter()
 
     _ = c[0, 0].item()
-    return end - start
+    return (end - start)/niter
 
 
 print(f"matrix size = {matrix_size} x {matrix_size}")
 
-cpu_time = benchmark("cpu", warmup=10, niter=100)
+cpu_time = benchmark("cpu", warmup=warmup, niter=niter)
 print(f"cpu time  = {cpu_time:.6f} s")
 
 if torch.cuda.is_available():
-    gpu_time = benchmark("cuda", warmup=10, niter=100)
+    gpu_time = benchmark("cuda", warmup=warmup, niter=niter)
     print(f"gpu time  = {gpu_time:.6f} s")
     print(f"speedup   = {cpu_time / gpu_time:.2f}x")
 else:
