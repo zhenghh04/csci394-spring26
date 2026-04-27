d046d0084a2fea5f39c9863b3e0a30ea887afdc1
Huihuo Zheng <zhenghh04@gmail.com>
2026-04-27 08:22:12 -0500

    Update run.sh

--- 08_machine_learning/exercises/3d_parallel/run.sh
+++ 08_machine_learning/exercises/3d_parallel/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+module use /soft/modulefiles
 module load conda
 conda activate
 mpiexec -np 8 --ppn 4 --cpu-bind depth -d 16 ./launcher.sh python3 train_transformer_3d_parallel.py --tp-size 1 --pp-size 1
