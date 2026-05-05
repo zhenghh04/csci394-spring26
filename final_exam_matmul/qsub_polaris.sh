24613b0460ba0f48b9f760a68e729417dc6b1b58
Huihuo Zheng <zhenghh04@gmail.com>
2026-05-05 09:02:23 -0500

    fix the sub

--- final_exam_matmul/qsub_polaris.sh
+++ final_exam_matmul/qsub_polaris.sh
@@ -3,11 +3,9 @@
 #PBS -l select=1:system=polaris
 #PBS -l walltime=00:30:00
 #PBS -l filesystems=home:eagle
-#PBS -q R7115122
+#PBS -q debug
 
 cd ${PBS_O_WORKDIR}
-module use /soft/modulefiles
-module load nvhpc/23.3
 
 make clean && make
 
